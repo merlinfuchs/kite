8aea5b85e474e9c52e880dc7a9b322f17475258e
Merlin <merlinfuchs2001@gmail.com>
2024-05-15 11:03:03 +0200

    create separate code workspaces

--- kite-sdk-flow/crates/kiteflow-compiler/src/main.rs
+++ kite-sdk-flow/crates/kiteflow-compiler/src/main.rs
@@ -1,1 +1,98 @@
-fn main() {}
+use std::{
+    env, fs,
+    io::{Read, Write},
+    rc::Rc,
+    sync::OnceLock,
+};
+
+use anyhow::{anyhow, Result};
+use binaryen::{CodegenConfig, Module};
+use std::io;
+use wasi_common::{pipe::ReadPipe, WasiCtx};
+use wasmtime::Linker;
+use wasmtime_wasi::WasiCtxBuilder;
+use wizer::Wizer;
+
+static mut WASI: OnceLock<WasiCtx> = OnceLock::new();
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let input_file = args.get(1).ok_or(anyhow!("No input file specified"))?;
+    let output_file = args.get(2).ok_or(anyhow!("No output file specified"))?;
+    let optimize = args.get(3).map(|s| s == "--optimize").unwrap_or(false);
+
+    let flow = {
+        if input_file == "-" {
+            let mut flow = Vec::new();
+            io::stdin().read_to_end(&mut flow)?;
+            flow
+        } else {
+            fs::read(input_file)?
+        }
+    };
+
+    let wasm = generate_module(flow, optimize)?;
+
+    if output_file == "-" {
+        io::stdout().write_all(&wasm)?;
+    } else {
+        fs::write(output_file, wasm)?;
+    }
+
+    Ok(())
+}
+
+fn generate_module(flow: Vec<u8>, optimize: bool) -> Result<Vec<u8>> {
+    let wasm = include_bytes!(concat!(env!("OUT_DIR"), "/engine.wasm"));
+
+    let wasi = WasiCtxBuilder::new()
+        .stdin(Box::new(ReadPipe::from(flow)))
+        .inherit_stdout()
+        .inherit_stderr()
+        .build();
+
+    // We can't move the WasiCtx into `make_linker` since WasiCtx doesn't implement the `Copy` trait.
+    // So we move the WasiCtx into a mutable static OnceLock instead.
+    // Setting the value in the `OnceLock` and getting the reference back from it should be safe given
+    // we're never executing this code concurrently. This code will also fail if `generate` is invoked
+    // more than once per execution.
+    if unsafe { WASI.set(wasi) }.is_err() {
+        panic!("Failed to set WASI static variable")
+    }
+
+    let wasm = Wizer::new()
+        .make_linker(Some(Rc::new(|engine| {
+            let mut linker = Linker::new(engine);
+            wasmtime_wasi::add_to_linker(&mut linker, |_ctx: &mut Option<WasiCtx>| {
+                unsafe { WASI.get_mut() }.unwrap()
+            })?;
+            Ok(linker)
+        })))?
+        .init_func("_start")
+        .wasm_bulk_memory(true)
+        .run(wasm)
+        .map_err(|_| anyhow!("Flow compilation failed"))?;
+
+    if optimize {
+        // This is more or less equivalent to running `wasm-opt -O4 --strip` on the engine module beforehand.
+
+        let codegen_cfg = CodegenConfig {
+            optimization_level: 4, // Aggressively optimize for speed.
+            shrink_level: 0,       // Don't optimize for size at the expense of performance.
+            debug_info: false,
+        };
+
+        let mut module = Module::read(&wasm)
+            .map_err(|_| anyhow!("Unable to read wasm binary for wasm-opt optimizations"))?;
+
+        module.optimize(&codegen_cfg);
+        module
+            .run_optimization_passes(vec!["strip"], &codegen_cfg)
+            .map_err(|_| anyhow!("Running wasm-opt optimization passes failed"))?;
+
+        Ok(module.write())
+    } else {
+        Ok(wasm)
+    }
+}
--- kite-sdk-flow/crates/kiteflow-engine/src/main.rs
+++ kite-sdk-flow/crates/kiteflow-engine/src/main.rs
@@ -4,17 +4,18 @@ use anyhow::Result;
 use engine::{FlowData, FlowTree};
 
 mod engine;
+mod sys;
 
 fn main() -> Result<()> {
     let mut flow_data = String::new();
-    io::stdin().read_to_string(&mut flow_data)
+    io::stdin()
+        .read_to_string(&mut flow_data)
         .expect("Failed to read flow data from stdin");
 
-    let flow: FlowData = serde_json::from_str(&flow_data)
-        .expect("Failed to parse flow data");
+    let flow: FlowData = serde_json::from_str(&flow_data).expect("Failed to parse flow data");
 
     let tree = FlowTree::new(&flow.nodes, &flow.edges);
-    
+
     println!("Parsed: {}", tree.entries.len());
 
     for entry in tree.entries {
@@ -22,4 +23,20 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+#[export_name = "kite_describe"]
+pub extern "C" fn describe() {}
+
+#[export_name = "kite_handle"]
+pub extern "C" fn handle(length: u32) {}
+
+#[export_name = "kite_get_api_version"]
+pub extern "C" fn get_api_version() -> u32 {
+    return 0;
+}
+
+#[export_name = "kite_get_api_encoding"]
+pub extern "C" fn get_api_encoding() -> u32 {
+    return 0;
 }
