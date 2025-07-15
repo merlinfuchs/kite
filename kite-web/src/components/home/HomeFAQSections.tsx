813112f852fa2ddd9d06c24ba8f963e863583203
merlinfuchs <merlinfuchs2001@gmail.com>
2025-07-15 18:27:45 +0200

    make faq SEO friendly

--- kite-web/src/components/home/HomeFAQSections.tsx
+++ kite-web/src/components/home/HomeFAQSections.tsx
@@ -5,42 +5,37 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import env from "@/lib/env/client";
+import { ChevronDown } from "lucide-react";
 
 interface FAQProps {
   question: string;
   answer: string;
-  value: string;
 }
 
 const FAQList: FAQProps[] = [
   {
     question: "Is Kite free to use?",
     answer: "Yes. Kite is open source and free to use for everyone.",
-    value: "item-1",
   },
   {
     question: "Can I customize the name and avatar of my Discord bot?",
     answer:
       "Yes. With Kite you create the bot yourself so you can customize the name and avatar however you like.",
-    value: "item-2",
   },
   {
     question: "Does Kite support slash commands and other Discord features?",
     answer:
       "Kite currently supports slash commands, message components, and event listeners. You can respond to slash commands and message components, and run actions based on events. We are working on adding more features!",
-    value: "item-3",
   },
   {
     question: "How many servers can I add my bot to?",
     answer:
       "You can add your bot to up to 100 servers. This limit may change in the future.",
-    value: "item-4",
   },
   {
     question: "Can I create multiple bots?",
     answer:
       "Yes. You can create up to 10 bots with Kite. This limit may change in the future.",
-    value: "item-5",
   },
 ];
 
@@ -55,14 +50,8 @@ export default function HomeFAQSection() {
       </h2>
 
       <Accordion type="single" collapsible className="w-full AccordionRoot">
-        {FAQList.map(({ question, answer, value }: FAQProps) => (
-          <AccordionItem key={value} value={value}>
-            <AccordionTrigger className="text-left">
-              {question}
-            </AccordionTrigger>
-
-            <AccordionContent>{answer}</AccordionContent>
-          </AccordionItem>
+        {FAQList.map(({ question, answer }: FAQProps) => (
+          <FAQItem key={question} question={question} answer={answer} />
         ))}
       </Accordion>
 
@@ -78,5 +67,20 @@ export default function HomeFAQSection() {
         </a>
       </h3>
     </section>
+  );
+}
+
+function FAQItem({ question, answer }: FAQProps) {
+  return (
+    <details className="text-left border-b [&_svg]:open:-rotate-180">
+      <summary className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline cursor-pointer">
+        <div>{question}</div>
+        <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      </summary>
+
+      <div className="overflow-hidden text-sm transition-all">
+        <p className="pb-4 pt-0">{answer}</p>
+      </div>
+    </details>
   );
 }
