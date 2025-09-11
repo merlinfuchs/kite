addfe576429dc58edcfd8a5dc07ad7f306abec1a
merlinfuchs <merlinfuchs2001@gmail.com>
2025-09-11 09:41:55 -0600

    integrate plausible for analytics

--- kite-web/next.config.mjs
+++ kite-web/next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["lucide-react"],
   output: process.env.OUTPUT === "export" ? "export" : "standalone",
+
+  async rewrites() {
+    return [
+      {
+        source: "/pl.js",
+        destination:
+          "https://insights.xenon.bot/js/script.outbound-links.pageview-props.revenue.tagged-events.js",
+      },
+      {
+        source: "/api/event",
+        destination: "https://insights.xenon.bot/api/event",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- kite-web/src/pages/_app.tsx
+++ kite-web/src/pages/_app.tsx
@@ -9,6 +9,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "@/lib/api/client";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import PlausibleProvider from "next-plausible";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,17 +24,23 @@ export default function App({ Component, pageProps }: AppProps) {
           font-family: ${fontSans.style.fontFamily};
         }
       `}</style>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider attribute="class">
-          <TooltipProvider delayDuration={200}>
-            <Component {...pageProps} />
-            <Toaster position="top-right" richColors={true} />
+      <PlausibleProvider
+        domain="kite.onl"
+        customDomain="https://insights.xenon.bot"
+        scriptProps={{ src: "/pl.js" }}
+      >
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider attribute="class">
+            <TooltipProvider delayDuration={200}>
+              <Component {...pageProps} />
+              <Toaster position="top-right" richColors={true} />
 
-            <SpeedInsights />
-            <Analytics />
-          </TooltipProvider>
-        </ThemeProvider>
-      </QueryClientProvider>
+              <SpeedInsights />
+              <Analytics />
+            </TooltipProvider>
+          </ThemeProvider>
+        </QueryClientProvider>
+      </PlausibleProvider>
     </>
   );
 }
